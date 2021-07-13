33b347fce3ec4bac229c23956aff7186e348efbf
Sébastien <stanneux@b2pweb.com>
2021-07-13 16:09:07 +0200

    fix: fixing usage of non php 7.1 function

--- src/Query/Expression/Match.php
+++ src/Query/Expression/Match.php
@@ -2,6 +2,6 @@
 
 namespace Bdf\Prime\Query\Expression;
 
-trigger_deprecation(__NAMESPACE__, '1.2', 'Match class is now deprecated. Use FullTextMatch instead of.');
+@trigger_error("Since ".__NAMESPACE__." 1.2: Match class is now deprecated. Use FullTextMatch instead of.", E_USER_DEPRECATED);
 
 class_alias(FullTextMatch::class, __NAMESPACE__.'\\Match');
