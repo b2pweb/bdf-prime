ce15f10fc2bfe64fc6bf87fd94aa74d81ca73d31
Vincent Quatrevieux <vquatrevieux@b2pweb.com>
2023-03-17 16:26:39 +0100

    [FRAM-87] Replace a where clause (#67)
    * feat: Replace a where clause (#FRAM-87)
    
    * fix: Where replace with null value
    
    * chore: Update changelog for #FRAM-87

--- tests/Query/QueryTest.php
+++ tests/Query/QueryTest.php
@@ -1610,4 +1610,22 @@ class QueryTest extends TestCase
             $query->toSql()
         );
     }
+
+    public function test_whereReplace()
+    {
+        $query = $this->query()->whereReplace('id', 1);
+
+        $this->assertEquals('SELECT * FROM test_ WHERE id = 1', $query->toRawSql());
+        $this->assertEquals('SELECT * FROM test_ WHERE id = 3', $query->whereReplace('id', 3)->toRawSql());
+        $this->assertEquals('SELECT * FROM test_ WHERE id = 3 AND id < 42', $query->whereReplace('id', '<', 42)->toRawSql());
+        $this->assertEquals('SELECT * FROM test_ WHERE id = 3 AND id < 42 AND raw clause', $query->whereRaw('raw clause')->toRawSql());
+    }
+
+    public function test_whereReplace_null()
+    {
+        $query = $this->query()->whereReplace('id', null);
+
+        $this->assertEquals('SELECT * FROM test_ WHERE id IS NULL', $query->toRawSql());
+        $this->assertEquals('SELECT * FROM test_ WHERE id = 42', $query->whereReplace('id', 42)->toRawSql());
+    }
 }
